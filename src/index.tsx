2de0b40cc31662d05698dfe52fa0956083332f26
KvashninAlexander2023 <study_python@mail.ru>
2024-03-30 14:06:12 +0500

    add test 02

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { splitIntoWords } from "./test/01-hello-test/01";
+
+const sentense = "Hello my friends";
+let result = splitIntoWords(sentense);
+// console.log(result);
+console.log(result[0] === "hello");
+console.log(result[1] === "my");
+console.log(result[2] === "friends");
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
